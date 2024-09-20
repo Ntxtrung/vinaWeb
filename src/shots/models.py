a66dc4d6a79d20b7f4db2261e653cfedaa91e702
ntxtrung <ntxtrung@gmail.com>
2024-09-20 13:40:06 +0700

    upate models Shot

--- src/shots/models.py
+++ src/shots/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from clients.models import Client, Job
 from django.db import models
 from django.utils.text import slugify
@@ -19,7 +21,7 @@ class Project(ItemsBase):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.project_name
+        return self.name
 
 
 class Package(ItemsBase):
@@ -27,7 +29,7 @@ class Package(ItemsBase):
     slug = models.SlugField(blank=True)
 
     def __str__(self):
-        return f"{self.package_name} - {self.created_at}"
+        return f"{self.name} - {self.created_at}"  # Corrected to use self.name
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -36,12 +38,18 @@ class Package(ItemsBase):
 
 
 class Shot(ItemsBase):
-    package = models.ForeignKey(Package, on_delete=models.CASCADE)
-    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
-    shot_id = models.CharField(max_length=36, blank=True)
-
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name="shots")
+    job = models.ForeignKey(
+        Job, on_delete=models.SET_NULL, null=True, blank=True, related_name="shots"
+    )
+    shot_id = models.CharField(max_length=36, unique=True, editable=False)
     word_ref = models.TextField(blank=True)
-    annotations = models.ImageField(upload_to="annotations/{name}", blank=True)
+    annotations = models.ImageField(upload_to="annotations/", blank=True)
 
     def __str__(self):
-        return f"{self.shot_name} - {self.package.name} - {self.package.project.name}"
+        return f"{self.name} - {self.package.name} - {self.package.project.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.shot_id:
+            self.shot_id = str(uuid.uuid4())
+        super().save(*args, **kwargs)
