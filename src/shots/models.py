72e90f6581ca6842e11bd8d18a42a52bb10f4064
ntxtrung <ntxtrung@gmail.com>
2024-09-20 13:30:48 +0700

    update models Shot

--- src/shots/models.py
+++ src/shots/models.py
@@ -1,45 +1,47 @@
+from clients.models import Client, Job
 from django.db import models
 from django.utils.text import slugify
-
-from clients.models import Client, Job
 
 # Create your models here.
 
 
-class Project(models.Model):
-    project_name = models.CharField(max_length=200)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+class ItemsBase(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+class Project(ItemsBase):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.project_name
 
 
-class Package(models.Model):
-    package_name = models.CharField(max_length=200, unique=True)
+class Package(ItemsBase):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     slug = models.SlugField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.package_name} - {self.created_at}"
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.package_name)
+            self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
 
-class Shot(models.Model):
+class Shot(ItemsBase):
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
     shot_id = models.CharField(max_length=36, blank=True)
-    shot_name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+
+    word_ref = models.TextField(blank=True)
+    annotations = models.ImageField(upload_to="annotations/{name}", blank=True)
 
     def __str__(self):
-        return f"{self.shot_name} - {self.package.package_name} - {self.package.project.project_name}"
+        return f"{self.shot_name} - {self.package.name} - {self.package.project.name}"
